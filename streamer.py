47e93442c8f3591db5210d0b40d7286972472c6c
LovedayBrooke <katmatfield@gmail.com>
2013-09-15 12:53:42 +0100

    Make a post request when receiving a DM

--- streamer.py
+++ streamer.py
@@ -1,6 +1,8 @@
 import os
 
 import twitter
+import requests
+
 
 auth = twitter.OAuth(
             consumer_key=os.environ['CONSUMER_KEY'],
@@ -15,8 +17,10 @@ twitter_userstream = twitter.TwitterStream(auth=auth,
 for msg in twitter_userstream.user():
     if 'direct_message' in msg:
     	if msg['direct_message']['sender_screen_name'] != "shelvd":
-	    	message = ("I got this too! "
-	    		"({0})").format(msg['direct_message']['text'])
+	    	input_text = msg['direct_message']['text']
+            message = ("I got this too! ({0})").format(input_text)
 	    	t = twitter.Twitter(auth=auth)
 	        t.direct_messages.new(user=os.environ['TWITTER_HANDLE'],
 	        	text=message)
+            requests.post("http://shelvd.herokuapp.com/", data={
+                "bkinput": input_text, "source": "twitter"})
